d2933252d649bd9c0887d8a92d1745ddacbc6ad8
arutaneo <68456723+arutaneo@users.noreply.github.com>
2020-08-02 20:52:29 +0900

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,6 @@
+$(function() {
+    $("#header").load("../include/header.html");
+    $("#side").load("../include/side.html");
+    $("#sub").load("../include/sub.html");
+    $("#footer").load("../include/footer.html");
+});
