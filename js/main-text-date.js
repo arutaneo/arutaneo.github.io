7b4c03957d4d674c5e79130f9a20aa047d3a77ce
arutaneo <68456723+arutaneo@users.noreply.github.com>
2020-08-09 12:38:02 +0900

    Delete main-text-date.js

--- js/main-text-date.js
+++
@@ -1,6 +0,0 @@
-var day = new Date(document.lastModified);
-var y = day.getFullYear();
-var m = day.getMonth() + 1;
-var d = day.getDate();
-
-document.write("Last Update: " + y + "年" + m + "月" + d + "日");
